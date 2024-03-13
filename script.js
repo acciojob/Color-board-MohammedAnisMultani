25e14b58776260e54676336fe43f7248cf63e3f1
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-13 20:20:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const squares = 500;
+
+for(let i=0; i<squares; i++){
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.addEventListener("mouseover", () => addColor(square))
+    square.addEventListener("mouseout", () => removeColor(square))
+container.append(square);
+}
+
+function addColor(e){
+    let color = randomColor();
+    e.style.background = color;
+    e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+function removeColor(e){
+    e.style.background = "#1d1d1d";
+    e.style.boxShadow = '0 0 2px #000'
+}
+
+
+function randomColor(){
+     return colors[Math.floor(Math.random()*colors.length)]
+     
+}
