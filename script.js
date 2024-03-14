1393c2e10d913719a4aa6bb0f7c4ceb3a3b67e97
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-14 11:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const squares = 500;
+const squares = 800;
 
 for(let i=0; i<squares; i++){
     const square = document.createElement("div")
